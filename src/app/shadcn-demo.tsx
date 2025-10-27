8616dcc0043e5da1e12d3f288ecd0cdc93d9778b
nguyenngocdue <due.nguyen@coe.surbana.tech>
2025-10-27 10:07:06 +0700

    feat: create a new project

---
+++ src/app/shadcn-demo.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function ShadcnDemo() {
+  return (
+    <div className="flex flex-col items-center justify-center py-8">
+      <Button>shadcn/ui Button</Button>
+    </div>
+  );
+}
